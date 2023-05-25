98d4d9085d50672ae5f89bb6480984d2c7a58a3c
Andy <lee.task.tw@gmail.com>
2023-05-25 18:01:58 +0800

    專案初始結構建立

---
+++ README.md
@@ -0,0 +1,5 @@
+# 商城專案項目實作
+## 使用技術
+### 前端：HTML、CSS、JavaScript、Vue.js
+### 後端：Spring Boot、Mybatis、Spring Cloud
+### 資料庫：MySQL、Redis
---
+++ mall-order/src/test/java/com/leetasktw/mallorder/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetasktw.mallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
